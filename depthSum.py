101d07d88a87a1acaa722f094c2cc2bd49c3d148
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-11-04 18:36:25 -0500

    Create depthSum.py

---
+++ depthSum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    
+    num=0
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+       
+        self.getSum(0,1,nestedList)
+        return self.num
+    
+    def getSum(self,sum,level,item):
+        
+        if type(item)==list:
+            for x in item:
+                self.getSum(sum,level,x)
+        
+        elif item.isInteger():
+            self.num=self.num+ item.getInteger()*(level)
+        
+        else:
+            self.getSum(sum,level+1,item.getList())
+                
+  
+ 
