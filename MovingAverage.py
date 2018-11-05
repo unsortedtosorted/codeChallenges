27444d51291a23c305580e54b6edb01a26403a0e
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-11-04 21:40:42 -0500

    Update MovingAverage.py

--- MovingAverage.py
+++ MovingAverage.py
@@ -1,3 +1,16 @@
+"""
+No looping required, faster than 99.87% python solutions
+
+1, sum=1,[1], return 1
+10 sum=11, [1,10] return (11/2)
+3 sum=14 [1,10,3] return 14/3
+5 sum=19 [1,10,3,5] d[4-3-1]=1 19-1, sum 18, return 18/3
+4 sum=22 [1,10,3,5,4] d[5-3-1]=d[1]=10 22-10=12 return 12/3=4
+
+
+"""  
+
+
 class MovingAverage(object):
 
     size=0
