13b96870cb011b02e76a42c37ab97cbf19c1ed28
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-11-12 22:28:03 -0500

    Update deleteDuplicates.py

--- deleteDuplicates.py
+++ deleteDuplicates.py
@@ -1,8 +1,39 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+"""
+Runs 100% faster than all python submission
+
+maintain 2 pointer 
+curr starts at head
+next start from head.next
+
+if list is: 
+
+2 2 3 4 5 5 
+
+Check curr.val and next.val, 2==2
+        curr.next=next.next
+        next=curr.next
+list= 2 3 4 5 5
+
+curr at 2, next at 3
+curr.val!=next.val, 2!=3
+    curr=curr.next
+    next=next.next
+
+curr at 3, next at 4
+curr.val!=next.val, 3!=4
+    curr=curr.next
+    next=next.next
+
+curr at 5, next at 5
+curr.val!=next.val, 5==5
+    curr.next=next.next=None
+    next=curr.next=None
+   
+next is None:
+  break 
+return head
+
+"""
 
 class Solution(object):
     def deleteDuplicates(self, head):
