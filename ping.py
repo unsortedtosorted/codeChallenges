cd801e447bcd9ec509dfcddc7e5d7c7e3956125a
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-11-15 23:26:55 -0500

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,22 @@
+class RecentCounter(object):
+    l=[]
+    lastStart=0
+    def __init__(self):
+        self.l=[]
+        self.lastStart=0
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.l.append(t)
+        end=t
+        start=t-3000
+        if start<0:
+            return len(self.l)
+        else:
+            for x in range(self.lastStart,len(self.l)):
+                if self.l[x]>=start:
+                    self.lastStart=x
+                    return len(self.l[self.lastStart:])
+        
