2a33e06fe5720d57184fee1d59d3149853bd1664
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-12-21 21:58:15 -0500

    Create countSegments.py

---
+++ countSegments.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i=0
+        
+        if len(s)<1:
+            return 0
+        prev= " "==s[0]
+
+        for x in range(1,len(s)):
+            if not prev and s[x]==" ":
+                i+=1
+                prev=True
+            elif s[x]!=" ":
+                prev=False
+                
+        if prev:
+            return i
+        else:return (i+1)
