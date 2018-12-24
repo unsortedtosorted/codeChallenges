91f88e4068b9b4519082c79aa7cd67c1211d3a9b
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-12-24 11:57:56 -0500

    Create minDiffInBST.py

---
+++ minDiffInBST.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minDiffInBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        l=[None,None]
+        r=[]
+        def inOrder(root):
+            if root:
+                inOrder(root.left)
+                r.append(root.val)
+                if l[0]==None:
+                    l[0]=(root.val)
+                else:
+                    temp=abs(root.val-l[0])
+                    if l[1]==None:
+                        l[1]=temp
+                    else:
+                        l[1]=min(temp,l[1])
+                    l[0]=root.val
+                    
+                inOrder(root.right)
+           
+        
+        inOrder(root)
